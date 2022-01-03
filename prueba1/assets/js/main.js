e401d457faa8ca9af3d2f8517222d2bfac36ef41
TSYF <tsyf1999@gmail.com>
2022-01-02 22:10:24 -0300

    Arreglo de la responsividad de las cartas. Anclas y tooltips a los iconos del footer

---
+++ prueba1/assets/js/main.js
@@ -0,0 +1,6 @@
+// Activación de tooltips de bootstrap
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
