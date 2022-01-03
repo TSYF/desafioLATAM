e65cbd008d8d573dd85fc4b1d6e1af8b5a197e08
TSYF <tsyf1999@gmail.com>
2022-01-03 10:19:23 -0300

    Arregle las cartas y mejore la navbar con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,12 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+$(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 50) {
+    $('.navbar').css('background','var(--viajes)');
+    } else {
+    $('.navbar').css('background','transparent');
+    }
+    });
+})
